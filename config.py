671cb7a43288dfc48ab5a3489203c4cc258f9a95
Mrabhi108 <ankinmasum@gmail.com>
2024-09-27 09:09:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY Tencent
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7867147604:AAEr80n-139JGxIz63UmIWQwnnL7y0LhsIo")
+BOT_TOKEN=("7588346706:AAEz6UZx8XQAqzIeMrOcf88lk9F2_A5VRMg")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("KingPatcherHackofficial")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1352339080")
+ADMIN_IDS=("6819117533")
 
 
 
