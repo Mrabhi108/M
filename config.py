a22988fee263eb1eac5ad8278b817e8b51e8db1a
Mrabhi108 <ankinmasum@gmail.com>
2024-09-27 07:51:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Tencent
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7867147604:AAEr80n-139JGxIz63UmIWQwnnL7y0LhsIo")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("KingPatcherHackofficial")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1352339080")
+
+
+
+
+
+  
